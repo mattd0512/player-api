b47e7d8ddba8f133243697acd0ddc6c28a1d6898
iceagelemonade <bryanjcarr88@gmail.com>
2022-10-29 21:44:46 -0400

    adds subdoc for users to list there usernames on various platforms

---
+++ mongoose-express-auth-API-boilerplate-main/app/scripts/scripts.js
@@ -0,0 +1,11 @@
+// const express = require('express')
+// const game = require('../models/game')
+// const Game = require('../models/game')
+
+// const findGame = (apiId) = {
+    
+// }
+
+// if (!Game.find({apiId})) {
+    
+// }
