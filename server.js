2b349869c0ab776630d1ea3500aebc402b499ca1
iceagelemonade <bryanjcarr88@gmail.com>
2022-10-31 15:39:56 -0400

    added routes for reviews

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const gameRoutes = require('./app/routes/game_routes')
+const reviewRoutes = require('./app/routes/review_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -69,6 +70,7 @@ app.use(requestLogger)
 app.use(exampleRoutes)
 app.use(gameRoutes)
 app.use(userRoutes)
+app.use(reviewRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
